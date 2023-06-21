ff4404750ef4fdac091e31f399607f2a0b4e0411
Martin Alturria <martinalturria@hotmail.com>
2023-06-21 13:17:30 -0300

    Agrego Form

--- lib/user.dart
+++ lib/user.dart
@@ -1,67 +1,112 @@
 import 'package:flutter/material.dart';
 import 'package:auth0_flutter/auth0_flutter.dart';
-import 'package:sample/detail_screen.dart';
-import 'dart:convert';
-import 'package:http/http.dart' as http;
 
-class UserWidget extends StatelessWidget {
+class UserWidget extends StatefulWidget {
   final UserProfile? user;
 
   const UserWidget({required this.user, final Key? key}) : super(key: key);
 
-  void postBack() async {
-    final username = user?.name;
-    final email = user?.email;
+  @override
+  _UserWidgetState createState() => _UserWidgetState();
+}
 
-    // Realizar la solicitud HTTP POST al backend
-    final response = await http.post(
-      Uri.parse('http://localhost:3024/auth/register'),
-      headers: {'Content-Type': 'application/json'},
-      body: jsonEncode({
-        'name': username,
-        'email': email,
-      }),
-    );
+class _UserWidgetState extends State<UserWidget> {
+  final List<String> empresas = [
+    'Empresa A',
+    'Empresa B',
+    'Empresa C',
+    'Empresa D',
+  ];
 
-    if (response.statusCode == 200) {
-      print('Se agregaron exitosamente');
-    } else {
-      print('No se pudo agregar los datos');
-    }
-  }
+  String? selectedEmpresa;
 
   @override
   Widget build(BuildContext context) {
-    postBack();
-    final pictureUrl = user?.pictureUrl;
-    // id, name, email, email verified, updated_at
-    return Column(crossAxisAlignment: CrossAxisAlignment.start, children: [
-      if (pictureUrl != null)
-        Container(
+    final pictureUrl = widget.user?.pictureUrl;
+
+    return Column(
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: [
+        if (pictureUrl != null)
+          Container(
             margin: const EdgeInsets.only(bottom: 12),
             child: CircleAvatar(
               radius: 56,
               child: ClipOval(child: Image.network(pictureUrl.toString())),
-            )),
-      Card(
-          child: Column(children: [
-        UserEntryWidget(propertyName: 'Id', propertyValue: user?.sub),
-        UserEntryWidget(propertyName: 'Name', propertyValue: user?.name),
-        UserEntryWidget(propertyName: 'Email', propertyValue: user?.email),
-        UserEntryWidget(
-            propertyName: 'Email Verified?',
-            propertyValue: user?.isEmailVerified.toString()),
-        UserEntryWidget(
-            propertyName: 'Updated at',
-            propertyValue: user?.updatedAt?.toIso8601String()),
-      ])),
-      ElevatedButton(
-          onPressed: () {
-            Navigator.of(context).push(
-                MaterialPageRoute(builder: (context) => const DetailScreen()));
-          },
-          child: const Text('Probando Detail'))
-    ]);
+            ),
+          ),
+        Form(
+          child: Column(
+            children: [
+              DropdownButtonFormField<String>(
+                decoration: const InputDecoration(labelText: 'Empresa'),
+                value: selectedEmpresa,
+                onChanged: (newValue) {
+                  setState(() {
+                    selectedEmpresa = newValue;
+                  });
+                },
+                items: empresas.map((empresa) {
+                  return DropdownMenuItem<String>(
+                    value: empresa,
+                    child: Text(empresa),
+                  );
+                }).toList(),
+              ),
+              TextFormField(
+                decoration:
+                    const InputDecoration(labelText: 'Número de teléfono'),
+                keyboardType: TextInputType.phone,
+                validator: (value) {
+                  if (value == null || value.isEmpty) {
+                    return 'Por favor ingresa el número de teléfono';
+                  }
+                  return null;
+                },
+                onChanged: (value) {
+                  // Guardar el valor del campo de texto
+                  // Puedes usar el valor guardado para otros fines
+                },
+              ),
+              TextFormField(
+                decoration: const InputDecoration(labelText: 'Dirección'),
+                validator: (value) {
+                  if (value == null || value.isEmpty) {
+                    return 'Por favor ingresa la dirección';
+                  }
+                  return null;
+                },
+                onChanged: (value) {
+                  // Guardar el valor del campo de texto
+                  // Puedes usar el valor guardado para otros fines
+                },
+              ),
+              TextFormField(
+                decoration: const InputDecoration(labelText: 'Licencia'),
+                validator: (value) {
+                  if (value == null || value.isEmpty) {
+                    return 'Por favor ingresa la Licencia';
+                  }
+                  return null;
+                },
+                onChanged: (value) {
+                  // Guardar el valor del campo de texto
+                  // Puedes usar el valor guardado para otros fines
+                },
+              ),
+              ElevatedButton(
+                onPressed: () {
+                  if (Form.of(context).validate()) {
+                    // Lógica adicional al presionar el botón "Enviar"
+                  }
+                },
+                child: const Text('Enviar'),
+              ),
+            ],
+          ),
+        ),
+      ],
+    );
   }
 }
 
@@ -69,17 +114,23 @@ class UserEntryWidget extends StatelessWidget {
   final String propertyName;
   final String? propertyValue;
 
-  const UserEntryWidget(
-      {required this.propertyName, required this.propertyValue, final Key? key})
-      : super(key: key);
+  const UserEntryWidget({
+    required this.propertyName,
+    required this.propertyValue,
+    final Key? key,
+  }) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
     return Container(
-        padding: const EdgeInsets.all(6),
-        child: Row(
-          mainAxisAlignment: MainAxisAlignment.spaceBetween,
-          children: [Text(propertyName), Text(propertyValue ?? '')],
-        ));
+      padding: const EdgeInsets.all(6),
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+        children: [
+          Text(propertyName),
+          Text(propertyValue ?? ''),
+        ],
+      ),
+    );
   }
 }
